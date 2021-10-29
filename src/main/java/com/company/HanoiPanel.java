6e5f761d4864b4e765693aaa5f2adee12f5490e9
Eric Ye <yuxuan.smiley@gmail.com>
2021-10-28 18:05:38 -0700

    Code cleanup

--- src/main/java/com/company/HanoiPanel.java
+++ src/main/java/com/company/HanoiPanel.java
@@ -3,24 +3,60 @@ package com.company;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class HanoiPanel extends JPanel {
-    private static final Color POLE_COLOR = new Color(0x594700);
+
+    /**
+     * Font for step text
+     */
     private static final Font STEP_FONT = new Font("Monospace", Font.PLAIN, 15);
-    private static final char[] STEP_TEXT = new char[9];
-    static {
-        // initialize the first few chars
-        STEP_TEXT[0] = 'S';
-        STEP_TEXT[1] = 't';
-        STEP_TEXT[2] = 'e';
-        STEP_TEXT[3] = 'p';
-        STEP_TEXT[4] = ' ';
-    }
-    private ArrayList<Disk> stackA;
-    private final ArrayList<Disk> stackB = new ArrayList<>();
-    private final ArrayList<Disk> stackC = new ArrayList<>();
-    private final Disk[] disks;
+
+    /**
+     * Height of disk
+     */
+    public static final int D_HEIGHT = 20;
+
+    /**
+     * Width of smallest disk
+     */
+    public static final int INITIAL_WIDTH = 50;
+
+    /**
+     * How much the width increases with each successive disk
+     */
+    private static final int D_WIDTH_INC = 10;
+
+    /**
+     * Color of poles
+     */
+    private static final Color POLE_COLOR = new Color(0x594700);
+
+    /**
+     * Color of disks
+     */
+    private static final Color[] D_COLORS = {
+            new Color(0xff8e8e),
+            new Color(0xecb283),
+            new Color(0xd8ce77),
+            new Color(0xa7c56c),
+            new Color(0x72b261),
+            new Color(0x569f68),
+            new Color(0x4b8c78),
+            new Color(0x417179),
+            new Color(0x364a66),
+            new Color(0x2d2c52)
+    };
+
+    /**
+     * Number of disks
+     */
+    private final int nDisks;
+
+    private ArrayList<Integer> stackA;
+    private final ArrayList<Integer> stackB = new ArrayList<>();
+    private final ArrayList<Integer> stackC = new ArrayList<>();
+
     private final TowerOfHanoi solver;
 
     private int curr;
@@ -29,10 +65,7 @@ public class HanoiPanel extends JPanel {
         super();
         setBackground(Color.white);
         stackA = new ArrayList<>(n);
-        disks = new Disk[n];
-        for (int i = 0; i < n; i++) {
-            disks[i] = new Disk(n - i - 1);
-        }
+        nDisks = n;
         solver = new TowerOfHanoi(n);
         reset();
     }
@@ -58,7 +91,7 @@ public class HanoiPanel extends JPanel {
     private void next() {
         if (curr == solver.getSize() - 1) return;
         TowerOfHanoi.Step step = solver.getStep(++curr);
-        Disk curr = switch (step.from) {
+        int curr = switch (step.from) {
             case 'A' -> stackA.remove(stackA.size() - 1);
             case 'B' -> stackB.remove(stackB.size() - 1);
             case 'C' -> stackC.remove(stackC.size() - 1);
@@ -73,7 +106,7 @@ public class HanoiPanel extends JPanel {
     private void prev() {
         if (curr < 0) return;
         TowerOfHanoi.Step step = solver.getStep(curr--);
-        Disk curr = switch (step.to) {
+        int curr = switch (step.to) {
             case 'A' -> stackA.remove(stackA.size() - 1);
             case 'B' -> stackB.remove(stackB.size() - 1);
             case 'C' -> stackC.remove(stackC.size() - 1);
@@ -87,7 +120,8 @@ public class HanoiPanel extends JPanel {
     }
     public void reset() {
         curr = -1;
-        stackA = new ArrayList<>(Arrays.asList(disks));
+        // fill stackA with decreasing numbers
+        stackA = new ArrayList<>(IntStream.range(0, nDisks).map(i -> nDisks - i - 1).boxed().toList());
         stackB.clear();
         stackC.clear();
         repaint();
@@ -101,65 +135,38 @@ public class HanoiPanel extends JPanel {
         // draw poles
         g.setColor(POLE_COLOR);
         for (int i = 0, j = width / 4; i < 3; i++, j += width / 4) {
-            g.fillRoundRect(j, height - Disk.HEIGHT * 11, 5, Disk.HEIGHT * 12, 5, 5);
+            g.fillRoundRect(j, height - D_HEIGHT * 11, 5, D_HEIGHT * 12, 5, 5);
         }
         // draw disks
         for (int i = 0; i < stackA.size(); i++) {
-            stackA.get(i).draw(g, i, 0, width, height);
+            drawDisk(g, i, 0, width, height, stackA.get(i));
         }
         for (int i = 0; i < stackB.size(); i++) {
-            stackB.get(i).draw(g, i, 1, width, height);
+            drawDisk(g, i, 1, width, height, stackB.get(i));
         }
         for (int i = 0; i < stackC.size(); i++) {
-            stackC.get(i).draw(g, i, 2, width, height);
+            drawDisk(g, i, 2, width, height, stackC.get(i));
         }
         // draw step
         g.setColor(Color.black);
-        String number = Integer.toString(curr + 1);
-        for (int i = 0; i < number.length(); i++) {
-            STEP_TEXT[i + 5] = number.charAt(i);
-        }
         g.setFont(STEP_FONT);
-        g.drawChars(STEP_TEXT, 0, number.length() + 5, 5, 20);
+        g.drawString("Step " + (curr + 1), 5, 20);
     }
 
-    private static class Disk {
-        public static final int HEIGHT = 20;
-        public static final int INITIAL_WIDTH = 50;
-        private final int number;
-        private static final Color[] COLORS = {
-                new Color(0xff8e8e),
-                new Color(0xecb283),
-                new Color(0xd8ce77),
-                new Color(0xa7c56c),
-                new Color(0x72b261),
-                new Color(0x569f68),
-                new Color(0x4b8c78),
-                new Color(0x417179),
-                new Color(0x364a66),
-                new Color(0x2d2c52)
-        };
-        private final Color color;
-        private static final int WIDTH_INC = 10;
-        public Disk(int number) {
-            this.number = number;
-            color = COLORS[number];
-        }
-
-        /**
-         * Draws this rectangle
-         * @param g      graphics object
-         * @param num    height from bottom starting from 0
-         * @param stack  number from 0 to 2, the stack of this rectangle
-         * @param width  width of outer panel
-         * @param height height of outer panel
-         */
-        public void draw(Graphics g, int num, int stack, int width, int height) {
-            int x = width / 4 * (stack + 1) - (number * WIDTH_INC) - INITIAL_WIDTH / 2;
-            int rectWidth = number * WIDTH_INC * 2 + INITIAL_WIDTH;
-            g.setColor(color);
-            //noinspection SuspiciousNameCombination
-            g.fillRoundRect(x, height - num * HEIGHT - HEIGHT, rectWidth, HEIGHT, HEIGHT, HEIGHT);
-        }
+    /**
+     * Draws this disk
+     * @param g      graphics object
+     * @param num    height from bottom starting from 0
+     * @param stack  number from 0 to 2, the stack of this rectangle
+     * @param width  width of outer panel
+     * @param height height of outer panel
+     * @param diskNo the disk number, starting with 0 for the smallest
+     */
+    public void drawDisk(Graphics g, int num, int stack, int width, int height, int diskNo) {
+        int x = width / 4 * (stack + 1) - (diskNo * D_WIDTH_INC) - INITIAL_WIDTH / 2;
+        int rectWidth = diskNo * D_WIDTH_INC * 2 + INITIAL_WIDTH;
+        g.setColor(D_COLORS[diskNo]);
+        //noinspection SuspiciousNameCombination
+        g.fillRoundRect(x, height - num * D_HEIGHT - D_HEIGHT, rectWidth, D_HEIGHT, D_HEIGHT, D_HEIGHT);
     }
 }
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -7,7 +7,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.net.URL;
 
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
